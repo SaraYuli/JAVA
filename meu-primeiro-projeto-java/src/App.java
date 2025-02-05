84401537d92785e6665665c94b7e73a28891c3c2
Sara Yuli <sarayuli@uni9.edu.br>
2025-02-04 21:25:33 -0300

    Meu primeiro projeto Java (Dio)

---
+++ meu-primeiro-projeto-java/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá, Mundo!");
+        /*Quebrando a maldição rs... */
+    }
+}
